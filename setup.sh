10a9d993dbd805f7366eadd0a5c9a8dd2cfc3cd2
segin-GH <segin.bytes@gmail.com>
2023-04-20 11:04:15 +0530

    .

--- setup.sh
+++ setup.sh
@@ -21,6 +21,7 @@
 
 #sudo apt install redshift -y;
 #sudo apt install nitrogeni -y;
+#sudo apt install lxappearance -y;
 
 #sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg;
 #echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list;
