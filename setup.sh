5ee774c816366344dbf330a6620328a402c29ee6
segin-GH <segin.bytes@gmail.com>
2023-05-23 12:10:03 +0530

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
+## BASIC
 
 #sudo apt install vim -y;
 #sudo apt install i3 -y;
 #sudo apt install curl -y ;
+
+## KITY
 
 #curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n;
 #sudo ln -s ~/.local/kitty.app/bin/kitty /usr/local/bin/;
@@ -15,9 +18,10 @@
 #gio set ~/Desktop/kitty*.desktop metadata::trusted true;
 #chmod a+x ~/Desktop/kitty*.desktop;
 
-
 #cd ~/code/dot-files;
 #cp config /home/yui/.config/i3;
+
+## I3 SETUP 
 
 #sudo apt install redshift -y;
 #sudo apt install nitrogeni -y;
@@ -25,10 +29,14 @@
 #sudo apt install maim -y;
 #sudo apt isntall xclip -y;
 
+## BRAVE BROWSER
+
 #sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg;
 #echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list;
 #sudo apt update;
 #sudo apt install brave-browser -y;
+
+## VSCODE
 
 #sudo apt update;
 #curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg;
@@ -38,10 +46,14 @@
 #sudo apt update;
 #sudo apt install code;
 
+## GITHUB
+
 #sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
 #sudo apt-add-repository https://cli.github.com/packages
 #sudo apt update
 #sudo apt install gh
+
+## ZSH
 
 #sudo apt install zsh -y;
 # sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
@@ -52,7 +64,7 @@
 
 #git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k;
 
-#sudo reboot;
+## Pyenv setup
 
 #sudo apt-get update;
 #curl https://pyenv.run | zsh;
@@ -61,5 +73,18 @@
 #sleep 1;
 #exec $SHELL;
 #pyenv --version;
+
+## git
+
 #git config --global user.name "segin-GH"
 #git config --global user.email "segin.bytes@gmail.com"
+#git config --global diff.tool vimdiff
+#git config --global pull.rebase true
+
+
+## FZF
+
+#git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+#~/.fzf/install
+
+
